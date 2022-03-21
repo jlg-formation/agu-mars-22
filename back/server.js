54ac64c817994236ce05dfb850cde5be0ace0ed1
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-21 15:11:18 +0100

    dev: added library api date

--- back/server.js
+++ back/server.js
@@ -1,15 +1,23 @@
 console.log("Je demarre le serveur web.");
 
 const express = require("express");
+const serveIndex = require("serve-index");
+const api = require("./api");
 
 const port = 3000;
+const wwwDir = "./public";
 
 const app = express();
 
-app.get("/truc", (req, res) => {
+app.use((req, res, next) => {
   console.log("req: ", req.url);
-  res.status(234).send("hello");
+  next();
 });
+
+app.use("/api", api);
+
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir));
 
 app.listen(port, () => {
   console.log(`Server started successfully at port ${port}`);

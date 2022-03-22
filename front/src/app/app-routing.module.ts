252a65cd49818182592a12c0955f4cf71eb54dbc
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-22 14:06:37 +0100

    dev: routing step2
    We have configured the path and components.

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './routes/home/home.component';
+import { LegalComponent } from './routes/legal/legal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: '',
+  },
+  {
+    component: LegalComponent,
+    path: 'legal',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

bde09fb5734276e0076d51829aaa55bc4badcdf7
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-22 14:12:52 +0100

    dev: routing step3
    We have added router-outlet in the body

--- front/src/app/layout/layout.module.ts
+++ front/src/app/layout/layout.module.ts
@@ -1,24 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BodyComponent } from './body/body.component';
-
-
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    FooterComponent,
-    BodyComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-    BodyComponent
-  ]
+  declarations: [HeaderComponent, FooterComponent, BodyComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, FooterComponent, BodyComponent],
 })
-export class LayoutModule { }
+export class LayoutModule {}

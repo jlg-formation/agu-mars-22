b6aabf26884e7ca90474a8e97bf2d945ffc99797
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-23 16:46:06 +0100

    dev: example of asynchrone code

---
+++ async/await.js
@@ -0,0 +1,12 @@
+const { promises: fs } = require("fs");
+
+(async () => {
+  try {
+    const files = await fs.readdir(".");
+    console.log("files: ", files);
+    const content = await fs.readFile(files[0], { encoding: "utf-8" });
+    console.log("content: ", content);
+  } catch (err) {
+    console.log("err: ", err);
+  }
+})();

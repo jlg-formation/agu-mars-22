5d3774fe82c3d21cb3630fab747a6da23a53025a
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-23 09:27:40 +0100

    dev: added reactive form

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  f = new FormGroup({
+    name: new FormControl('qwerqwer'),
+    price: new FormControl(45),
+    qty: new FormControl(12),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

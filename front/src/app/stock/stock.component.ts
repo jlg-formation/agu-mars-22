c415e778e078a7a92eb61f75b15390fb037eb7e4
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-23 08:55:57 +0100

    dev: use javascript object for listing articles

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Article[] = [
+    {
+      name: 'Tournevis',
+      price: 2.34,
+      qty: 145,
+    },
+    {
+      name: 'Marteau',
+      price: 4.5,
+      qty: 23,
+    },
+    {
+      name: 'Pince',
+      price: 6,
+      qty: 12,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

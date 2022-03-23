cb90b1420e0d3ec5990bb5b84220f1c34492e01e
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-23 13:48:04 +0100

    dev: added post article on back-end

--- back/api.ts
+++ back/api.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const articles = [{ id: 1, name: "Pinceau", price: 3.21, qty: 456 }];
 
@@ -10,6 +11,15 @@ app.get("/date", (req, res) => {
 
 app.get("/articles", (req, res) => {
   res.json(articles);
+});
+
+app.use(express.json());
+
+app.post("/articles", (req, res) => {
+  const article = req.body;
+  article.id = uuidv4();
+  articles.push(article);
+  res.status(201).json(article);
 });
 
 export const api = app;
--- front/src/app/services/http-article.service.ts
+++ front/src/app/services/http-article.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Article } from '../interfaces/article';
 import { ArticleService } from './article.service';
 
+const url = 'http://localhost:3000/api/articles';
+
 @Injectable({
   providedIn: 'root',
 })
@@ -14,9 +16,24 @@ export class HttpArticleService extends ArticleService {
   }
 
   retrieveAll() {
-    this.http.get<Article[]>('http://localhost:3000/api/articles').subscribe({
+    this.http.get<Article[]>(url).subscribe({
       next: (articles) => {
         this.articles = articles;
+      },
+      error: (err) => {
+        console.log('err: ', err);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
+  }
+
+  override add(a: Article): void {
+    super.add(a);
+    this.http.post<void>(url, a).subscribe({
+      next: () => {
+        this.retrieveAll();
       },
       error: (err) => {
         console.log('err: ', err);

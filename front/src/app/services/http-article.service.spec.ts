37b65e05baffd29ba525588ab5a103564ef010df
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-24 15:47:37 +0100

    test: increase coverage

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -1,19 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { HttpArticleService } from './http-article.service';
 
+const url = 'http://localhost:3000/api/articles';
+
 describe('HttpArticleService', () => {
   let service: HttpArticleService;
+  let http: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(HttpArticleService);
+    http = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    http.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+    const req = http.expectOne(url);
+    expect(req.request.method).toBe('GET');
+    req.flush([]);
+  });
+
+  it('should be created but with error on retrieveAll', () => {
+    expect(service).toBeTruthy();
+    const req = http.expectOne(url);
+    expect(req.request.method).toBe('GET');
+    req.flush('', { status: 500, statusText: 'Internal Error' });
   });
 });

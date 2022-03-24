6283076c9f9d793a2c8cc432a16e2973c3734341
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2022-03-24 15:19:16 +0100

    test: ng test compile and test ok

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,35 +1,47 @@
-import { TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
+import { Router, Routes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
+import { HomeComponent } from './routes/home/home.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
 
 describe('AppComponent', () => {
+  let router: Router;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule.withRoutes(routes), LayoutModule],
+      declarations: [AppComponent],
     }).compileComponents();
+
+    router = TestBed.inject(Router);
+    fixture = TestBed.createComponent(AppComponent);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('front');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should render the home component', fakeAsync(() => {
     fixture.detectChanges();
+    router.initialNavigation();
+    tick();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('front app is running!');
-  });
+    const str = compiled.querySelector('main h1')?.textContent;
+    expect(str).toContain('Gérez efficacement votre stock.');
+  }));
 });
